dceb4c260a1043412904caa0afbdc04d9a069dcb
0xperp <0xperp@protonmail.com>
2024-08-09 12:02:40 -0400

    news sites

--- popup.js
+++ popup.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const timeline = document.getElementById('timeline');
     const single = document.getElementById('single');
+    const article = document.getElementById('article');
     const count = document.getElementById('count');
     const save = document.getElementById('save');
     const status = document.getElementById('status');
@@ -9,10 +10,12 @@ document.addEventListener('DOMContentLoaded', function() {
     chrome.storage.sync.get({
         timelineThreshold: 0.85,
         singleTweetThreshold: 0.803,
+        articleThreshold: 0.81,
         marketCount: 1
     }, function(items) {
         timeline.value = items.timelineThreshold;
         single.value = items.singleTweetThreshold;
+        article.value = items.articleThreshold;
         count.value = items.marketCount;
     });
 
@@ -20,11 +23,13 @@ document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('save').addEventListener('click', function() {
         var timelineThreshold = parseFloat(document.getElementById('timeline').value);
         var singleTweetThreshold = parseFloat(document.getElementById('single').value);
+        var articleThreshold = parseFloat(document.getElementById('article').value);
         var marketCount = parseInt(document.getElementById('count').value);
 
         chrome.storage.sync.set({
             timelineThreshold: timelineThreshold,
             singleTweetThreshold: singleTweetThreshold,
+            articleThreshold: articleThreshold,
             marketCount: marketCount
         }, function() {
             var status = document.getElementById('status');
